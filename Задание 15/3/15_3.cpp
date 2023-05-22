7ac667b9c5bc28faf4a872c32060c96b2df3f541
KriegKorpse <85568009+KriegKorpse@users.noreply.github.com>
2023-05-22 21:47:00 +0300

    Add files via upload

---
+++ Задание 15/3/15_3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Set.h"
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    Set<int> s(5);
+    s.Get(1);
+    s.Add(1);
+    s.Add(2);
+    s.Add(3);
+    s.Add(4);
+    s.Add(5);
+    cout << "Set: " << s << endl;
+    s.Get(10);
+    cout << "Set: " << s << endl;
+    return 0;
+}
